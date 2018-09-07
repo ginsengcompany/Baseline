e2baad9f1cf19b02f2007b61095e0a805c2e99a4
Antonio Valente <antosaverio.valente@gmail.com>
2018-09-07 09:46:08 +0200

    rimosso tutto

--- src/main/java/it/baseline/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package it.baseline.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
